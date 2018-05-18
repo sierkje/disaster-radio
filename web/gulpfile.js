5a03751bcca6383cdb4b75ddde4f22d5ec70e258
Sierk Beij <sierk@riseup.net>
2018-05-18 13:50:04 -0700

    Added sass globbing using gulp.js.

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -2,11 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
 var cssimport = require('gulp-cssimport');
 var inlineSource = require('gulp-inline-source');
 var js = require('./bin/build.js');
 
-gulp.task('build:js', js.build); 
+gulp.task('build:js', js.build);
 
 gulp.task('watch:js', js.watch);
 
@@ -14,14 +15,15 @@ gulp.task('watch:js', js.watch);
 gulp.task('hot:js', js.hot);
 
 gulp.task('build:css', function() {
-  return gulp.src('./src/css/*.scss')
+  return gulp.src('./src/index.scss')
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(cssimport())
     .pipe(gulp.dest('./static/build'));
 });
- 
+
 gulp.task('watch:css', function() {
-  gulp.watch('./src/css/**/*.scss', ['build:css']);
+  gulp.watch('./src/**/*.scss', ['build:css']);
 });
 
 // not currently used
