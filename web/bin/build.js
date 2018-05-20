4c95e10a14741b626be91888d196fe4f8ca29784
Sierk Beij <sierk@riseup.net>
2018-05-20 12:31:21 -0700

    Fixed eslint warnings and errors.

--- web/bin/build.js
+++ web/bin/build.js
@@ -1,135 +1,116 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var colors = require('colors');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var hmr = require('browserify-hmr');
-var minimist = require('minimist');
+/* eslint-disable no-console */
+
+const fs = require('fs')
+const path = require('path')
+const colors = require('colors')
+const browserify = require('browserify')
+const watchify = require('watchify')
+const hmr = require('browserify-hmr')
+const minimist = require('minimist')
 
 function getTime() {
-  var d = new Date;
-  var t = [d.getHours(), d.getMinutes(), d.getSeconds()];
-  t = t.map(function(i) {
-    return (i < 9) ? '0'+i : i;
-  })
-  return t[0] + ':' + t[1] + '.' + t[2];
+  const d = new Date()
+  let t = [d.getHours(), d.getMinutes(), d.getSeconds()]
+  t = t.map(i => ((i < 9) ? `0${i}` : i))
+  return `${t[0]}:${t[1]}.${t[2]}`
 }
 
 function build(opts) {
-  opts = opts || {};
-
-  var output = path.join(__dirname, '..', 'static', 'build', 'bundle.js');
+  const options = opts || {}
+  const output = path.join(__dirname, '..', 'static', 'build', 'bundle.js')
+  const doBrowserify = browserify({
+    entries: [path.join(__dirname, '..', 'src', 'index.js')],
+    cache: {},
+    packageCache: {},
+  })
 
   function onBuildEnd(msg) {
-    console.log("Completed".green + ((msg) ? (': ' + msg) : ''));
+    console.log(colors.green('Completed') + ((msg) ? (`: ${msg}`) : ''))
   }
 
   function onBuildStart() {
+    const outStream = fs.createWriteStream(output)
 
-    process.stdout.write("Build started at " + getTime() + "... ");
+    process.stdout.write(`Build started at ${getTime()}... `)
 
-    var outStream = fs.createWriteStream(output);
+    if (!options.dev) outStream.on('close', onBuildEnd)
 
-    if(!opts.dev) {
-      outStream.on('close', onBuildEnd);
-    }
+    doBrowserify.bundle().on('error', (error) => {
+      const isSyntaxError = (error instanceof SyntaxError)
+      const errorMatches = (error.message.match(/while parsing file/))
+      if (!isSyntaxError && !errorMatches) {
+        console.error(error)
+        return
+      }
 
-    b.bundle()
+      // Format syntax error messages nicely
+      const re = new RegExp(`${error.filename}:? ?`)
+      let msg = error.message.replace(re, '')
+      msg = msg.replace(/ while parsing file:.*/, '')
+      console.error()
+      console.error(`\n${colors.red('Error')}: ${msg.underline}`)
+      console.error()
+      console.error('Filename:', error.filename)
+      console.error()
+      console.error(error.loc)
+      console.error()
+      console.error(error.codeFrame)
+      console.error()
+    })
 
-      .on('error', function(err) {
-        if(err instanceof SyntaxError && err.message.match(/while parsing file/)) {
-          // Format syntax error messages nicely
-          var re = new RegExp(err.filename+'\:? ?');
-          var msg = err.message.replace(re, '');
-          msg = msg.replace(/ while parsing file\:.*/, '');
-          console.error();
-          console.error("\nError: ".red + msg.underline);
-          console.error();
-          console.error("Filename:", err.filename);
-          console.error();
-          console.error(err.loc);
-          console.error();
-          console.error(err.codeFrame);
-          console.error();
-        } else {
-          console.error(err);
-        }
-
-      })
-
-      .pipe(outStream);
+    doBrowserify.bundle().pipe(outStream)
   }
 
-  var b = browserify({
-    entries: [path.join(__dirname, '..', 'src', 'index.js')],
-    cache: {},
-    packageCache: {}
+  if (options.dev) {
+    console.log(colors.yellow('Watching for changes...'))
+    doBrowserify.plugin(watchify)
+  }
+
+  if (options.hot) {
+    console.log(colors.yellow('Hot module reloading enabled'))
+    doBrowserify.plugin(hmr)
+  }
+
+  doBrowserify.on('update', () => onBuildStart())
+
+  if (options.dev) doBrowserify.on('log', onBuildEnd)
+
+  doBrowserify.transform('babelify', {
+    presets: ['es2015'],
+    plugins: [['transform-react-jsx', { pragma: 'h' }]],
   })
 
-  if(opts.dev) {
-    console.log("Watching for changes...".yellow);
-    b.plugin(watchify);
-  }
-
-  if(opts.hot) {
-    console.log("Hot module reloading enabled".yellow);
-    b.plugin(hmr);
-  }
-
-  b.on('update', function(time) {
-    onBuildStart();
-  });
-
-  if(opts.dev) {
-    b.on('log', onBuildEnd);
-  }
-
-  b.transform('babelify', {
-    presets: [
-      'es2015'
-    ],
-    plugins: [
-      ['transform-react-jsx', {pragma: 'h'}]
-    ]
-  })
-
-  onBuildStart();
+  onBuildStart()
 }
 
-if (require.main === module) {
+(function doBuild() {
+  if (require.main === module) {
+    const argv = minimist(process.argv.slice(2), {
+      alias: { d: 'dev' },
+      boolean: ['dev', 'hot'],
+      default: {},
+    })
 
-  var argv = minimist(process.argv.slice(2), {
-    alias: {
-      d: 'dev'
-    },
-    boolean: [
-      'dev',
-      'hot'
-    ],
-    default: {}
-  });
-
-  build(argv);
-
-} else {
+    build(argv)
+    return
+  }
 
   module.exports = {
-    build: build,
+    build,
+    watch: (opts) => {
+      const options = opts || {}
+      options.dev = true
 
-    watch: function(opts) {
-      opts = opts || opts;
-      opts.dev = true;
-      return build(opts);
+      return build(options)
     },
+    hot: (opts) => {
+      const options = opts || {}
+      options.dev = true
+      options.hot = true
 
-    hot: function(opts) {
-      opts = opts || opts;
-      opts.dev = true;
-      opts.hot = true;
-      return build(opts);
-    }
+      return build(options)
+    },
   }
-
-}
+}())
