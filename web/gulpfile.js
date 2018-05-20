c7460d27a555495f5e9e71a161c8d326afee1d27
Sierk Beij <sierk@riseup.net>
2018-05-20 03:12:49 -0700

    Added stylelint for linting SCSS files.

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -7,6 +7,7 @@ const autoprefixer = require('gulp-autoprefixer');
 var cssimport = require('gulp-cssimport');
 var inlineSource = require('gulp-inline-source');
 var eslint = require('gulp-eslint');
+const stylelint = require('gulp-stylelint');
 var js = require('./bin/build.js');
 
 gulp.task('build:js', js.build);
@@ -36,6 +37,12 @@ gulp.task('watch:css', function() {
   gulp.watch('./src/**/*.scss', ['build:css']);
 });
 
+gulp.task('lint:css', () => gulp.src('src/**/*.scss')
+  .pipe(stylelint({
+    failAfterError: true,
+    reporters: [{ formatter: 'string', console: true }],
+  })));
+
 // not currently used
 gulp.task('build:html', function() {
   return gulp.src('./static/index.html')
@@ -48,7 +55,7 @@ gulp.task('build', ['build:js', 'build:css']);
 
 gulp.task('watch', ['watch:js', 'watch:css']);
 
-gulp.task('lint', ['lint:js']);
+gulp.task('lint', ['lint:js', 'lint:css']);
 
 gulp.task('hot', ['hot:js', 'watch:css']);
 
