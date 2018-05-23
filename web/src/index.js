1b6d509fa4c97eb09db8ffdaa0de29defefb5cb3
Sierk Beij <sierk@riseup.net>
2018-05-23 10:20:49 -0700

    Refactored src/index.js.

--- web/src/index.js
+++ web/src/index.js
@@ -1,55 +1,41 @@
 
-import {h, render, Component as PreactComponent} from 'preact'
-import Socket from './core/lib/js/socket'
+import { h, render, Component as PreactComponent } from 'preact'
 import ashnazg from 'ashnazg'
+import Socket from './core/lib/js/socket'
 import cipher from './core/lib/js/cipher'
+import makeChat from './apps/chat'
+import chatActions from './apps/chat/actions'
 
 const Component = ashnazg(PreactComponent)
-var Chat = require('./apps/chat')(Component)
-
-function renderAll() {
-  var container = document.getElementById('container');
-  var replace;
-
-  if(container.children.length) {
-    replace = container.children[0];
-  }
-
-  render(<Chat state />, container, replace);
-}
-
-
-
+const Chat = makeChat(Component)
+const container = document.getElementById('container')
 
 function init() {
-  app.socket = new Socket('/ws', {debug: true});
+  /* eslint-disable-next-line global-require */
+  app.actions = chatActions
+  app.socket = new Socket('/ws', { debug: true })
+  app.socket.connect((err, isConnected) => {
+    /* eslint-disable-next-line no-console */
+    console.log('connected:', isConnected)
+    /* eslint-disable-next-line no-console */
+    if (err) console.error(err)
+  })
 
-  app.socket.connect(function(err, isConnected) {
+  render(<Chat state />, container, (container.children.length)
+    ? container.children[0]
+    : undefined)
 
-    console.log("connected:", isConnected);
-    if(err) console.error(err);
-
-  });
-
-  app.actions = require('./apps/chat/actions');
-
-  renderAll();
   try {
-    cipher.init();
-  } catch(e) {
-    console.error(e);
-    alert("Fatal error:", e);
+    cipher.init()
+  } catch (e) {
+    /* eslint-disable-next-line no-console */
+    console.error(e)
+    /* eslint-disable-next-line no-alert */
+    alert('Fatal error:', e)
   }
 }
 
-
-
 // hot module reloading
-if(module.hot) {
-  module.hot.accept();
-}
+if (module.hot) module.hot.accept()
 
-init();
-
-
-
+init()
